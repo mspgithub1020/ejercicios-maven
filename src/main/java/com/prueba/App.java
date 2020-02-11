1491d9caa0ab703c3fde37aa704e08b201d3ade8
Mario <mspformacion1020@gmail.com>
2020-02-11 13:57:41 +0100

    modificado

--- src/main/java/com/prueba/App.java
+++ src/main/java/com/prueba/App.java
@@ -1,5 +1,7 @@
 package com.prueba;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,87 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+
+        Pelicula peli = new Pelicula("Unico", "drama", "clint eastwood", "Warner");
+
+        peli.iniciar();
+
+
+
+
+
     }
 }
+
+interface IReproducible{
+
+    public void iniciar();
+    public void pausar();
+    public void retroceder();
+    public void avanzar();
+
+}
+
+abstract class Contenido{
+
+    private String titulo;
+    private String descripcion;
+    private String director;
+    private String productora;
+
+    public Contenido(String titulo,String descripcion,String director,String productora){
+
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.director = director;
+        this.productora = productora;
+
+
+    }
+
+}
+
+class Pelicula extends Contenido implements IReproducible{
+
+
+    public Pelicula (String titulo,String descripcion,String director,String productora){
+
+        super(titulo,descripcion,director,productora);
+
+    }
+
+    public void iniciar(){System.out.println("inicio peli");}
+    public void pausar(){}
+    public void retroceder(){}
+    public void avanzar(){}
+
+}
+
+class Serie extends Contenido{
+
+
+    private ArrayList<Capitulo> capitulos = new ArrayList<Capitulo>();
+
+
+
+    public Serie (String titulo,String descripcion,String director,String productora){
+
+        super(titulo,descripcion,director,productora);
+
+    }
+
+}
+
+class Capitulo implements IReproducible{
+
+    public void iniciar(){}
+    public void pausar(){}
+    public void retroceder(){}
+    public void avanzar(){}
+
+
+}
+
+
+
